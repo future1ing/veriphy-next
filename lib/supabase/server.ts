f5ba057cc52fb0046cc5a7929dd29955e9a388ee
future1ing <hafida.taoufiq.01@gmail.com>
2026-06-12 16:46:04 +0100

    fix: add types to setAll cookiesToSet

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -9,11 +9,11 @@ export function createClient() {
     {
       cookies: {
         getAll() { return cookieStore.getAll() },
-        setAll(cookiesToSet: any) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }: any) =>
+        setAll(cookiesToSet: { name: string; value: string; options: any }[]) {
+           try {
+            cookiesToSet.forEach(({ name, value, options }) => {
               cookieStore.set(name, value, options)
-            )
+            })
           } catch {}
         },
       },
